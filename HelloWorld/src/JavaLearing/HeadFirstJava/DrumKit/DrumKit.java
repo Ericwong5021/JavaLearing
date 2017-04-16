b7d565901ac35fa65a4de79caa38388271664cd0
Ericwong <970699442@qq.com>
2017-04-16 10:00:42 +0800

    test

--- HelloWorld/src/JavaLearing/HeadFirstJava/DrumKit/DrumKit.java
+++ HelloWorld/src/JavaLearing/HeadFirstJava/DrumKit/DrumKit.java
@@ -6,10 +6,10 @@ package DrumKit;
 class DrumKit {
     boolean topHat = true;
     boolean snare = true;
-
     void playTopHat () {
         System.out.println("ding ding da-ding");
     }
+
     void PlaySnare () {
         System.out.println("bang bang ba-bang");
     }
